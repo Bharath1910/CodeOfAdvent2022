bfcbc4393152b5ff53cd50a549af86c99827c847
Bharath1910 <av.bharath@proton.me>
2022-12-04 23:54:25 +0530

    completed part 2 day 4

---
+++ Dec 4/two.py
@@ -0,0 +1,20 @@
+with open("Dec 4/input.txt") as f:
+    score = 0
+    data = f.read().splitlines()
+    
+    for i in data:
+        seperatedTestCases = i.split(",")
+
+        firstRange = [int(i) for i in seperatedTestCases[0].split("-")]
+        secondRange = [int(i) for i in seperatedTestCases[1].split("-")]
+
+        firstSetRange = set(range(firstRange[0], firstRange[1] + 1))
+        secondSetRange = set(range(secondRange[0], secondRange[1] + 1))
+
+        print(firstSetRange.intersection(secondSetRange))
+        print()
+
+        if len(firstSetRange.intersection(secondSetRange)) != 0:
+            score += 1
+
+print(score)
